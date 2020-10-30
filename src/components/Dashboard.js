ceffadd07eedf5e33b95d92ca55c31633e69baef
Ania <58440008+szczepanka@users.noreply.github.com>
2020-10-30 11:51:58 +0100

    Header +Dashboard start

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header.js";
+
+function Dashboard(){
+    return(
+       <div> {<Header /> }
+        <h1> Dashboard </h1>
+        </div> 
+    )
+}
+
+export default Dashboard;

c57a875408009bdd012ce6c47eab0de63feec7cc
Ania <58440008+szczepanka@users.noreply.github.com>
2020-10-30 13:04:36 +0100

    circle

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,16 @@
 
 import React from "react";
-import Header from "./Header.js";
 import "./Dashboard.css";
 
 function Dashboard(){
     return(
-       <div className="Dashboard"> {<Header /> }
+       <div className="Dashboard"> 
        <div className="circle">
-        <p className="textIn">
-            <h1> You saved </h1>
-            <h2> ****** *** CO2</h2>
-        </p>
-
+            <p className="text"> You saved </p> 
+            <p className="text"> ****** *** CO2</p>
        </div>
-
-        </div> 
+       </div> 
     )
 }
 
+export default Dashboard;

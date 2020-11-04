a0bfe07aa449930ac0984e4f0b436f16e06dbe8a
Ania <58440008+szczepanka@users.noreply.github.com>
2020-11-04 15:48:25 +0100

    challanges work

--- src/components/ChooseChallange.js
+++ src/components/ChooseChallange.js
@@ -4,6 +4,7 @@ import "./chooseChallange.css";
 
 function Challange() {
     return(
+        <div className="challangesContainer">
         <div className="challanges">
             <h1>Choose New Challange</h1>
             <button className="challangeBtn">Eat Vegetarian (31days)</button>
@@ -11,6 +12,7 @@ function Challange() {
             <button className="challangeBtn">Recycle your plastic bottles (31days)</button>
             <button className="challangeBtn">Use public transport to commute (31days)</button>
             <button className="challangeBtn">Don't fly an airplane (365days)</button>
+        </div>
         </div>
     );
 }

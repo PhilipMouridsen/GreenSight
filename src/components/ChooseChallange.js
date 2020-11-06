98ea83c20b8d51eca967fd059fca51728525c85f
Anna Wojciechowska <wojciechowska.anna2511@gmail.com>
2020-11-06 11:24:36 +0100

    tachangesuswerprofile

--- src/components/ChooseChallange.js
+++ src/components/ChooseChallange.js
@@ -6,7 +6,7 @@ function Challange() {
     return(
         <div className="challangesContainer">
         <div className="challanges">
-            <h1>Choose New Challange</h1>
+            <h1 className="newchallenge">Choose New Challange</h1>
             <button className="challangeBtn">Eat Vegetarian (31days)</button>
             <button className="challangeBtn">Take the bike to work (14days)</button>
             <button className="challangeBtn">Recycle your plastic bottles (31days)</button>

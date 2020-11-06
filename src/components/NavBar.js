4a06004cc9d9767c056e148aa902e22004fd5681
PhilipCM <pmou@itu.dk>
2020-11-06 11:59:49 +0100

    NavBar fix (?) :) :)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./navBar.css";
 import home from "./img/menu-home.png";
 import wrld from "./img/menu-world.png";
-import plus from "./img/menu-plus.png";
+import plus from "./img/menu+.png";
 import account from "./img/menu-account.png";
 import settings from "./img/menu-settings.png";
 import { Link } from "react-router-dom";

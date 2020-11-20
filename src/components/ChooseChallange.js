8aff561f1eb39368de8a5f51584fa6cac6b608c5
Ania <58440008+szczepanka@users.noreply.github.com>
2020-11-20 14:11:25 +0100

    Revert "shit-poop"
    This reverts commit e0074515edfae02a360bc906e616f479f7e8a8d1.

--- src/components/ChooseChallange.js
+++ src/components/ChooseChallange.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./chooseChallange.css";
 import Leaf from "./Leaf.js";
 import Dialog from "./Dialog";
 import { database }  from "../firebase"
-import Dialog2 from "./Dialog2";
 
 //takes the data correctly, next -> make the co2 consumption go to dashboard
 database.collection('Challenges').get().then((snapshot) => {
@@ -14,39 +13,37 @@ database.collection('Challenges').get().then((snapshot) => {
 
 //const [isOpen, toggled] = isOpen(false);
 
-function Challange (){
+class Challange extends Component {
+  state = {
+    isOpen: false,
+  };
 
 
-  const Pop = () => {
-    const [isPopped, setPop] = useState(false);
-    setPop(isPopped = true);
-    <Dialog2 />
-  }
-
+  render() {
     return (
       <div className="challanges">
         <h1 className="newchallenge">Choose New Challange</h1>
         <button
           className="challangeBtn"
-          onClick={Pop}
+          onClick={ (e) => this.setState({ isOpen: true })}
         >
           Eat Vegetarian (31days)
         </button>
-        <button className="challangeBtn" onClick={Pop}>Take the bike to work (14days)</button>
-        <button className="challangeBtn" onClick={Pop}>
+        <button className="challangeBtn" onClick={ (e) => this.setState({ isOpen: true })}>Take the bike to work (14days)</button>
+        <button className="challangeBtn" onClick={ (e) => this.setState({ isOpen: true })}>
           Recycle your plastic bottles (31days)
         </button>
-        <button className="challangeBtn" onClick={Pop}>
+        <button className="challangeBtn" onClick={ (e) => this.setState({ isOpen: true })}>
           Use public transport to commute (31days)
         </button>
-        <button className="challangeBtn"onClick={Pop}>
+        <button className="challangeBtn"onClick={ (e) => this.setState({ isOpen: true })}>
           Don't fly an airplane (365days)
         </button>
 
-       <div className="dialogBox" > <Dialog2 /> </div>  
+       <div className="dialogBox" > <Dialog isOpen={this.state.isOpen} onClose = {(e) => this.setState({isOpen: false})}>Do you accept the challange?</Dialog> </div>  
       </div>
     );
-
   }
+}
 
 export default Challange;

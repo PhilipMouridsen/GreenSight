c3425cf567cef243f5b73b9205f3635ab1318dc9
Ania <58440008+szczepanka@users.noreply.github.com>
2020-11-25 12:58:03 +0100

    working pop up + challange in console

--- src/components/ChooseChallange.js
+++ src/components/ChooseChallange.js
@@ -6,13 +6,9 @@ import Dialog2 from "./Dialog2";
 function Challange() {
   const [isPopped, setPop] = useState(false);
 
-  const pop = () => {
-    setPop(!isPopped);
-    console.log("pop")
-  };
-
 
   const plsWork = (e) =>{
+    setPop(!isPopped);
     const theOne = challs[e]
     console.log(theOne);
   }
@@ -42,19 +38,19 @@ function Challange() {
       {isPopped && <Dialog2 />}
       <div className="challanges">
         <h1 className="newchallenge">Choose New Challange</h1>
-        <button className="challangeBtn" onClick={plsWork(0)} >
+        <button className="challangeBtn" onClick={() => plsWork(0)} >
           {challs[0]} (31days)
         </button>
-        <button className="challangeBtn" onClick={plsWork(1)} >
+        <button className="challangeBtn" onClick={() => plsWork(1)} >
           {challs[1]} (14days)
         </button>
-        <button className="challangeBtn" onClick={pop} >
+        <button className="challangeBtn" onClick={() => plsWork(2)} >
           {challs[2]} (31days)
         </button>
-        <button className="challangeBtn" onClick={pop} >
+        <button className="challangeBtn" onClick={() => plsWork(3)} >
           {challs[3]} (31days)
         </button>
-        <button className="challangeBtn" onClick={pop} >
+        <button className="challangeBtn" onClick={() => plsWork(4)} >
           {challs[4]} (365days)
         </button>
       </div>

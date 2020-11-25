d9aba8ee0041cc6fc86e48e6a064d91339ba5f09
PhilipCM <pmou@itu.dk>
2020-11-25 14:09:42 +0100

    NavBar sort of centered :/

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,34 +10,44 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="NavBar">
-      <div className="icons">
-        <Link to="/"> 
-          <img src={home} alt="home-button" className="icon"/>
+      <div className="iconRow">
+        <div className="icons">
+          <Link to="/">
+            <img src={home} alt="home-button" className="icon" />
           </Link>
+        </div>
+
+        <div className="icons">
+          <Link to="/explore">
+            <img src={wrld} alt="wrld-button" className="icon" />
+          </Link>
+        </div>
+
+        <div className="icons">
+          <Link to="/Challange">
+            <img src={plus} alt="plus-button" className="icon" />
+          </Link>
+        </div>
+
+        <div className="icons">
+          <Link to="/userprofile">
+            <img src={account} alt="account-button" className="icon" />
+          </Link>
+        </div>
+
+        <div className="icons">
+          <Link to="/settings">
+            <img src={settings} alt="settigns-button" className="icon" />
+          </Link>
+        </div>
       </div>
 
-      <div className="icons">
-        <Link to="/explore">
-          <img src={wrld} alt="wrld-button" className="icon"/>
-          </Link>
-      </div>
-
-      <div className="icons">
-        <Link to="/Challange">
-          <img src={plus} alt="plus-button" className="icon"/>
-          </Link>
-      </div>
-
-      <div className="icons">
-        <Link to="/userprofile">
-          <img src={account} alt="account-button" className="icon"/>
-          </Link>
-      </div>
-
-      <div className="icons">
-        <Link to="/settings">
-          <img src={settings} alt="settigns-button" className="icon"/>
-          </Link>
+      <div className="iconText">
+        <h4 className="iconName" id="dashboard">Dashboard</h4>
+        <h4 className="iconName" id="explore">Explore</h4>
+        <h4 className="iconName" id="challenge">challenges</h4>
+        <h4 className="iconName" id="profile">Profile</h4>
+        <h4 className="iconName" id="settings">Settings</h4>
       </div>
     </div>
   );

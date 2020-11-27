5c517c27f80851662b0c8fadb29b32578ce5d967
PhilipCM <pmou@itu.dk>
2020-11-27 14:09:51 +0100

    Dialog 1!!

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,47 +2,29 @@ import React, { useState } from "react";
 import "./chooseChallange.css";
 import { Link } from "react-router-dom";
 
-const Close = () => {
-  setOpennes(false);
-};
-return (
-  <div className={isOpen ? "dialogBox" : "dialogHide"}>
-    <h3 id="header">Do you accept the challange?</h3>
-    <div className="guziki">
-      <Link to="/">
+function Dialog() {
+  const [isOpen, setOpennes] = useState(true);
+
+  const Close = () => {
+    setOpennes(false);
+  };
+  return (
+    <div className={isOpen ? "dialogBox" : "dialogHide"}>
+      <h3 id="header">Do you accept the challange?</h3>
+      <div className="guziki">
+        <Link to="/">
+          <button className="approvalButton" onClick={Close}>
+            {" "}
+            Approve{" "}
+          </button>
+        </Link>
         <button className="approvalButton" onClick={Close}>
           {" "}
-          Approve{" "}
+          Decline{" "}
         </button>
-      </Link>
-      <button className="approvalButton" onClick={Close}>
-        {" "}
-        Decline{" "}
-      </button>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
-const Close = () => {
-  setOpennes(false);
-};
-
-return (
-  <div className={isOpen ? "dialogBox" : "dialogHide"}>
-    <h3 id="header">Do you accept the challange?</h3>
-    <div className="guziki">
-      <Link to="/">
-        <button className="approvalButton" onClick={Close}>
-          {" "}
-          Approve{" "}
-        </button>
-      </Link>
-      <button className="approvalButton" onClick={Close}>
-        {" "}
-        Decline{" "}
-      </button>
-    </div>
-  </div>
-);
-
-export default Dialog2;
+export default Dialog;
